f64a839a0b2b78f87fa2eef169323e774831c0d1
Developer Beck <85181067+BeckSattorov@users.noreply.github.com>
2023-06-22 22:15:30 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Admin from './components/Admin';
+import { Hero } from './components/Hero';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Admin />} />
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/hero' element={<Hero />} />
       </Routes>
     </>
   );
