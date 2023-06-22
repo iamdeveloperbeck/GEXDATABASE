bd276880dcd2e21f51985a92e0b42cb9eacac317
Developer Beck <85181067+BeckSattorov@users.noreply.github.com>
2023-06-22 22:15:55 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Admin from './Admin'
 
 export const Hero = () => {
   return (
-    <div className='p-[0_60px_0_60px]'>
-      <div>Hero</div>
-    </div>
+    <>
+      <Admin />
+    </>
   )
 }
